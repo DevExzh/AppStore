8cf47e602cb61dd039d1b8a92714a943746d1dd2
Ryker Zhu <202283930036@nuist.edu.cn>
2022-12-20 18:38:32 +0800

    Simplify the Language enum and change the case

--- src/models/Language.java
+++ src/models/Language.java
@@ -1,18 +1,11 @@
 package models;
 
 public enum Language {
-    ENGLISH,
-    FRENCH,
-    ITALIAN,
-    JAPANESE,
-    KOREAN,
-    PORTUGUESE,
-    RUSSIAN,
-    SPANISH,
-    SWEDISH,
-    TURKISH,
-    CHINESE,
-    CZECH,
-    DANISH,
-    FINNISH,
+    English,
+    French,
+    Japanese,
+    Portuguese,
+    Russian,
+    Spanish,
+    Chinese
 }
